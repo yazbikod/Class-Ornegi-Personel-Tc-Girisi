270293d70a8917e9b13158dd2b3f3664391d4583
yazbikod <aslan_murathan@hotmail.com>
2022-10-25 01:23:27 +0300

    Proje dosyası ekle.

---
+++ Personel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Personel_Tc
+{
+    public class Personel
+    {
+        private string tcKimlikNo;
+
+        public string TCKİMLİKNO
+        {
+            get
+            {
+                return tcKimlikNo.Substring(0, 5) + "******";
+            }
+            set
+            {
+                bool kontrol = false;
+                if (value.Length == 11)
+                {
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        bool sayimi = char.IsNumber(value[i]);
+                        if (sayimi)
+                        {
+                        }
+                        else
+                        {
+                            kontrol = true;
+                            break;
+                        }
+                    }
+                    if (kontrol)
+                    {
+                        Console.WriteLine("Sadece Rakam Girişi Yapılmalıdır....");
+                    }
+                    else
+                    {
+                        tcKimlikNo = value;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Girilen Kimlik No 11 karakter olmalıdır....");
+                }
+
+            }
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using Class_Personel_Tc;
+
+Personel per = new Personel();
+
+Console.Write("Lütfen Kimlik Numaranızı Giriniz : ");
+
+per.TCKİMLİKNO = Console.ReadLine();
+
+Console.WriteLine("TC : " + per.TCKİMLİKNO);
